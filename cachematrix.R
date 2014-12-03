34fd0d3aa62c977f27ad7e86727788a7e0ab3954
chuanonn <tank_onn98@yahoo.com>
2014-12-03 12:09:00 +0800

    Coursera R Programming Assignment 2
    This reverts commit e4eed4153bd237a2dd4532fc3f40ce0e22fd0cb7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Functions below are used to stores a matrix and caches the calculated inverse matrix.
 
-## Write a short comment describing this function
+#Assumption: 
+#1.Matrix supplied is invertible.
+#2.Matrix supplied is square matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+#Construct list containing functions to
+#1.Set the value of the matrix
+#2.Get the value of the matrix
+#3.Set the value of the inverse matrix
+#4.Get the value of the inverse matrix
 
+makeCacheMatrix <- function(x) {
+  m <- NULL
+  
+  set <- function(y) {  
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  
+  list(set = set, 
+       get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+#Returns the inverse matrix of supplied matrix.
+#Calculates using solve() function if the inverse is not calculated.
+#Or return the inverse matrix from cache if its already been calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+#Testing using mock data.
+#x<-matrix(c(1,2,1,3),nrow=2,ncol=2);x
+
+# [,1] [,2]
+# [1,]    1    1
+# [2,]    2    3
+
+#y<-makeCacheMatrix(x)
+
+#cacheSolve(y)
+
+# [,1] [,2]
+# [1,]    3   -1
+# [2,]   -2    1
+
+#cacheSolve(y)
+
+#getting cached data
+# [,1] [,2]
+# [1,]    3   -1
+# [2,]   -2    1
